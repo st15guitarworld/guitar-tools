b4801f95fb11e2dd0d68ab59f8202f7c10a306a8
st15guitarworld <snoel0015@gmail.com>
2017-08-19 16:31:01 -0400

    Add fret accents to guitar

---
+++ src/Constants.jsx
@@ -0,0 +1,4 @@
+const Constants = {
+fretsWithAccents:[3,5,7,9]
+}
+export default Constants;
--- src/Fret.jsx
+++ src/Fret.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Constants from './Constants';
 
 export default class Fret extends Component {
   constructor(props) {
@@ -7,13 +8,23 @@ export default class Fret extends Component {
     this.generateNoteBubble = this.generateNoteBubble.bind(this);
     this.drawX = this.drawX.bind(this);
     this.drawFretNumber = this.drawFretNumber.bind(this);
+    this.drawFretAccent = this.drawFretAccent.bind(this);
   }
   drawFretNumber(){
     if (this.props.string == 6) {
       return <i className="fret-number">{this.props.fret}</i>
     }
   }
+drawFretAccent(){
+  let fretAccent = <div className="fret-accent circle"></div>
+  if(this.props.fret == 12 && (this.props.string == 3 || this.props.string == 5)) {
+  return fretAccent;
+  }
 
+  if(Constants.fretsWithAccents.indexOf(this.props.fret) > -1 && this.props.string == 4){
+    return fretAccent;
+  }
+}
   drawX(){
     if(this.props.fret === 0 && !this.props.stringIsSet){
       return (<span id="x">X</span>)
@@ -37,6 +48,7 @@ export default class Fret extends Component {
     return (
       <td className="fret">
         {this.drawFretNumber()}
+        {this.drawFretAccent()}
         {this.drawX()}
         <span className="fretArea" onClick={event => this.onFretClick(event)} />
         {this.generateNoteBubble()}
