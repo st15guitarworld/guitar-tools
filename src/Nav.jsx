52ec3007d2725cce5f22311acf0e68f5bec7399a
st15guitarworld <snoel0015@gmail.com>
2017-08-26 21:05:27 -0400

    fix mobile css

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-
+let linkStyle = {
+  display:'block'
+};
 export default class Nav extends Component {
   render() {
     return (
       <nav className="nav-container">
         <ul>
           <li>
-            <NavLink strict exact to="/" activeClassName="is-active">
+            <NavLink strict exact to="/" activeClassName="is-active" style={linkStyle}>
               <i className="flaticon-metronome"></i>
               Metronome
             </NavLink>
           </li>
           <li>
-            <NavLink to="/tuner" activeClassName="is-active">
+            <NavLink to="/tuner" activeClassName="is-active" style={linkStyle}>
             <i className="flaticon-tuning-fork-shaped"></i>
               Tuner
             </NavLink>
           </li>
           <li>
-            <NavLink to="/chord-finder" activeClassName="is-active">
+            <NavLink to="/chord-finder" activeClassName="is-active" style={linkStyle}>
             <i className="flaticon-music"></i>
-              Chord Finder
+              Chords
             </NavLink>
           </li>
           <li>
-            <NavLink to="/scale-viewer" activeClassName="is-active">
+            <NavLink to="/scale-viewer" activeClassName="is-active" style={linkStyle}>
               <i className="flaticon-musical-note-black-shape"></i>
-              Scale Viewer
+              Scales
             </NavLink>
           </li>
         </ul>
